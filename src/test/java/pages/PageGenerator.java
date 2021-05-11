075d77b2b817b027b6f1203b851ff57f02537026
Onur Baskirt <onur.baskirt@dnata.com>
2021-05-12 01:41:10 +0400

    try catch removed.

--- src/test/java/pages/PageGenerator.java
+++ src/test/java/pages/PageGenerator.java
@@ -12,19 +12,7 @@ public class PageGenerator {
 
     //JAVA Generics to Create and return a New Page
     public <TPage extends BasePage> TPage getPage(Class<TPage> pageClass) {
-        try {
-            //Initialize the Page with its elements and return it.
-            return PageFactory.initElements(driver, pageClass);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            try {
-                throw e;
-            }
-            catch (Exception e1) {
-                e1.printStackTrace();
-            }
-        }
-        return null;
+        //Initialize the Page with its elements and return it.
+        return PageFactory.initElements(driver, pageClass);
     }
 }
